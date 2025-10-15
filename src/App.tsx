9d33f7cf7d3db884498ada2f2970e198f771b1e1
Juan Pablo Sala <jpsala@gmail.com>
2025-10-15 20:45:57 -0300

    Add Tailwind CSS integration and update dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import './App.css'
 
 function App() {
-  return (
-    <>
-        Hello world
-    </>
-  )
+    return (
+        <>
+            <div className="m-2">
+                <div className=" bg-amber-200 text-red-500 inline-block px-3 py-2 rounded">Hello world</div>
+            </div>
+        </>
+    )
 }
 
 export default App
